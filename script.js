3e2002242270b086b5fa6aea1c67ce897ce52254
gweseloh <greg.weseloh@gmail.com>
2025-06-03 15:29:27 -0500

    Added fetch button functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// so much empty
+const fetchButton = document.getElementById("fetchButton");
+const postList = document.getElementById("postList");
+
+fetchButton.addEventListener("click", () => {
+    fetch("https://jsonplaceholder.typicode.com/posts").then((response) => response.json()).then((posts) => {
+        postList.innerHTML = "";
+        for(post of posts) {
+            const newPost = document.createElement("ul");
+            newPost.innerHTML = `
+            <li>Title: ${post.title}
+            <li>Body: ${post.body}
+            `;
+            postList.appendChild(newPost);
+        }
+    });
+});
